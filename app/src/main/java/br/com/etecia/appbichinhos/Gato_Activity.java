101d93c7e27387f07645361751c391725c533f5e
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-04-17 22:14:36 -0300

    Criei e finalizei a Activity do cachorro e do gato, e iniciei a declaração das string text para colocar nos layouts.

--- app/src/main/java/br/com/etecia/appbichinhos/Gato_Activity.java
+++ app/src/main/java/br/com/etecia/appbichinhos/Gato_Activity.java
@@ -3,11 +3,11 @@ package br.com.etecia.appbichinhos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Dog_Activity extends AppCompatActivity {
+public class Gato_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dog_layout);
+        setContentView(R.layout.gato_layout);
     }
 }
