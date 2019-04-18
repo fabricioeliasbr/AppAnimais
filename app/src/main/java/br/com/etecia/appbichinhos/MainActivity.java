0a182585d594be2b42b4229c3ff874da1acb70cd
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-04-17 21:27:55 -0300

    Corrigi o erro no arquivo png 'tubarao', em seguida, inseri ID em todas image views juntamente com o escutador de eventos onClick. Também foi criada a public void 'abrirDetalhes', na qual, quando uma image view for clicada, irá abrir uma Activity com os detalhes do bicho. Vou começar a criar as Activitys, começando pelo cachorro.

--- app/src/main/java/br/com/etecia/appbichinhos/MainActivity.java
+++ app/src/main/java/br/com/etecia/appbichinhos/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.etecia.appbichinhos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirDetalhes(View view) {
+        switch(view.getId()){
+            case R.id.idDog :
+                Intent intent = new Intent(MainActivity.this,Dog_Activity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
